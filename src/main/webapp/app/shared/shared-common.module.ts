51a8e35fd92a89807008f5195524c40aaf5165da
Vinicius Louzada Jacobsem <vinicius.lj@gmail.com>
2018-06-01 00:27:59 -0300

    Initial application generated by JHipster-4.14.4

---
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -0,0 +1,42 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import locale from '@angular/common/locales/pt';
+
+import {
+    BuscapdfSharedLibsModule,
+    JhiLanguageHelper,
+    FindLanguageFromKeyPipe,
+    JhiAlertComponent,
+    JhiAlertErrorComponent
+} from './';
+
+@NgModule({
+    imports: [
+        BuscapdfSharedLibsModule
+    ],
+    declarations: [
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ],
+    providers: [
+        JhiLanguageHelper,
+        Title,
+        {
+            provide: LOCALE_ID,
+            useValue: 'pt'
+        },
+    ],
+    exports: [
+        BuscapdfSharedLibsModule,
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ]
+})
+export class BuscapdfSharedCommonModule {
+    constructor() {
+        registerLocaleData(locale);
+    }
+}
